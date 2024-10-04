1ec132ce0e441252c8222ff6b09077a427e4d2ed
Kyle Xiang <cxiang@lcs.on.ca>
2024-10-04 12:17:19 -0400

    Added additional Text views to display all the computed properties.

--- GeometricFigures/Model/Circle.swift
+++ GeometricFigures/Model/Circle.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+protocol Describable {
+    var description: String { get }
+}
+
+struct Circle{
+    var radius: Double
+    var diameter: Double{
+        return radius * 2
+    }
+    var area: Double{
+        return Double.pi * radius * radius
+    }
+    var perimeter: Double{
+        return 2 * Double.pi * radius
+    }
+    var circumferene: Double{
+        return perimeter
+    }
+}
+
--- GeometricFigures/Views/CircleView.swift
+++ GeometricFigures/Views/CircleView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct CircleView: View {
+    
+    //MARK: Stored properties
+    
+   @State var currentCircle = Circle(radius: 10)
+    //MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            //Add an image
+            
+            //Label (describe what the slider is for)
+            Text("Radius")
+            //Slider control - to allow for user input
+            Slider(value: $currentCircle.radius, in: 1...100 , step: 1.0)
+            
+            //Label (show the current slider value)
+            Text("Radius is: \(currentCircle.radius.formatted())")
+            Text("Diameter is: \(currentCircle.diameter.formatted())")
+            Text("Area is: \(currentCircle.area.formatted())")
+            Text("Circumference is: \(currentCircle.circumferene.formatted())")
+        }
     }
 }
 
