8002c623e96c43e0d0524ba325fea788c30b1c16
Kyle Xiang <cxiang@lcs.on.ca>
2024-10-04 11:54:13 -0400

    Added Circle structure to our model, and made a basic view to control the radius.

---
+++ GeometricFigures/Model/Circle.swift
@@ -0,0 +1,8 @@
+//
+//  Circle.swift
+//  GeometricFigures
+//
+//  Created by Chujun Xiang on 2024-10-03.
+//
+
+import Foundation
---
+++ GeometricFigures/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometricFigures
+//
+//  Created by Chujun Xiang on 2024-10-03.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
